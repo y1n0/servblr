e9cfd446801113492dd240a9e3351fc4e879b8c8
Shee Liku Pis <badr2097@gmail.com>
2018-07-26 23:42:28 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="servblr",
+    version="0.0.1",
+    license="MIT",
+    author="y1n0",
+    author_email="y1n0@pm.me",
+    description="A Tumblr instant messages wrapper in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/y1n0/servblr",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
